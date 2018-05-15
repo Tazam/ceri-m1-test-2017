aca45f68fe9b5296ccf83329de98b2677d2f8cb0
Tazam <gatrail@hotmail.fr>
2018-05-15 15:07:53 +0200

    AJout Test EnvironmentProviderTest Ajout fichier ListAnimals.csv

--- src/test/java/fr/univavignon/rodeo/EnvironmentProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/EnvironmentProviderTest.java
@@ -3,6 +3,10 @@
  */
 package fr.univavignon.rodeo;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import fr.univavignon.rodeo.api.IEnvironmentProvider;
 import fr.univavignon.rodeo.imp.EnvironmentProvider;
 
@@ -16,5 +20,19 @@ public class EnvironmentProviderTest extends IEnvironmentProviderTest {
 	{
 		IEnvironmentProvider eRet = new EnvironmentProvider("testAnimals.csv");
 		return eRet;
+	}
+	
+	public EnvironmentProvider getTestInstance3()
+	{
+		EnvironmentProvider eRet = new EnvironmentProvider("testAnimals.csv");
+		return eRet;
+	}
+	
+	@Test
+	public void testAddAnimals()
+	{
+		EnvironmentProvider epr = getTestInstance3();
+		epr.addAnimal("Lion", "Lion", "Base", "Jungle 8", "0");
+		assertEquals(4,epr.getAvailableEnvironments().size());
 	}
 }
