74f6c6a104cdd4950e0963da2fa47a5c8145d7b2
Tazam <gatrail@hotmail.fr>
2018-05-15 14:17:58 +0200

    Same

--- src/test/java/fr/univavignon/rodeo/AnimalTest.java
+++ src/test/java/fr/univavignon/rodeo/AnimalTest.java
@@ -2,6 +2,8 @@ package fr.univavignon.rodeo;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import fr.univavignon.rodeo.api.IAnimal;
@@ -15,18 +17,27 @@ public class AnimalTest extends IAnimalTest{
 		return a;
 	}
 	
-	public Animal getTestInstance3()
+	public ArrayList<Animal> getTestInstance3()
 	{
 		Animal a = new Animal("aTest",3,true,true,true);
-		return a;
+		Animal a2 = new Animal("aTest2",2,false,true,false);
+		Animal a3 = new Animal("aTest",3,true,true,true);
+		
+		ArrayList<Animal> ret = new ArrayList<Animal>();
+		ret.add(a);
+		ret.add(a2);
+		ret.add(a3);
+		return ret;
 	}
 	
 	@Test
 	public void testContainsAnimals()
 	{
-		final Animal a1 = getTestInstance3();
-		final Animal a2 = getTestInstance3();
-		assertEquals(true,a1.equals(a2));
+		final ArrayList<Animal> l = getTestInstance3();
+		assertEquals(true,l.get(0).equals(l.get(0)));
+		assertEquals(true,l.get(0).equals(l.get(2)));
+		assertEquals(false,l.get(0).equals(l.get(1)));
+		
 	}
 
 
