3a222af0b115b30b75ce24ba7c58875e4e67508b
Tazam <gatrail@hotmail.fr>
2018-05-21 01:52:52 +0200

    Test SpecieLevel

--- src/test/java/fr/univavignon/rodeo/GameStateTest.java
+++ src/test/java/fr/univavignon/rodeo/GameStateTest.java
@@ -5,10 +5,16 @@ package fr.univavignon.rodeo;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import fr.univavignon.rodeo.api.IAnimal;
+import fr.univavignon.rodeo.api.SpecieLevel;
 import fr.univavignon.rodeo.imp.Animal;
 import fr.univavignon.rodeo.imp.GameState;
+import fr.univavignon.rodeo.imp.Specie;
 
 /**
  * @author Schmidt Gaëtan
@@ -69,6 +75,27 @@ public class GameStateTest extends IGameStateTest {
 		gs.exploreArea();
 		gs.catchAnimal(new Animal("a2",0,false,false,true));
 		assertEquals(66,gs.getProgression());
+	}
+	
+	@Test
+	public void specieLevelNoviceTest()
+	{
+		GameState gs = getTestInstance2();
+		List<IAnimal> animals= new ArrayList<IAnimal>();
+		animals.add(new Animal("a1",24,true,true,true));
+		animals.add(new Animal("a2",100,true,true,true));
+		animals.add(new Animal("a3",500,true,true,true));
+		Specie sp = new Specie("s1",3,animals);
+		gs.catchAnimal(new Animal("a1",24,false,false,true));
+		gs.exploreArea();
+		assertEquals(SpecieLevel.NOVICE,gs.getSpecieLevel(sp));
+		gs.catchAnimal(new Animal("a2",100,true,true,true));
+		assertEquals(SpecieLevel.WRANGLER,gs.getSpecieLevel(sp));
+		gs.exploreArea();
+		gs.catchAnimal(new Animal("a3",500,true,true,true));
+		assertEquals(SpecieLevel.MASTER,gs.getSpecieLevel(sp));
+		
+		
 	}
 
 }
